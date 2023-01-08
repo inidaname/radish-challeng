3e5cdab84abe26a38209264af5a085a6a9f9234d
Hassan Sani <saniyhassan@gmail.com>
2023-01-08 13:43:49 +0100

    feat: styling layouts components for footer and header

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,13 @@
 import { NextComponentType } from "next";
 
 const Footer: NextComponentType = () => {
-  return <>Footer</>
+  return (
+    <>
+      <footer className="h-24 flex justify-between items-center fixed bottom-0">
+        Built with love
+      </footer>
+    </>
+  )
 }
 
 export default Footer
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,15 @@
 import {NextComponentType} from "next";
 
 const Header: NextComponentType = () => {
-  return <>Header</>
+  return (
+    <>
+      <header className="md:container md:mx-auto h-24 flex flex-row justify-between items-center">
+        <h1 className="font-bold text-lg">Challenge</h1>
+        <p>We are talking</p>
+        <p>&nbsp;</p>
+      </header>
+    </>
+  )
 }
 
 export default Header
